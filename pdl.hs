55605fd99db44a6eac777cf0167ee3b8130edfc6
sevontheedge <sivthiago@hotmail.com>
2018-10-30 11:11:28 -0300

    novos testes

--- pdl.hs
+++ pdl.hs
@@ -5,7 +5,23 @@ executaTransicao :: [Int] -> [(Int, Int)] -> [Int]
 executaTransicao estadosAtuais transicoes = [snd x | x <- transicoes, elem (fst x) estadosAtuais]
 
 executaNTransicoes :: [Int] -> [(Int, Int)] -> [Int]
-executaNTransicoes estadosAtuais transicoes = 
-    if listasIguais (concatenarDuasListasSemRepeticao (executaTransicao estadosAtuais transicoes) estadosAtuais)  estadosAtuais
-        then estadosAtuais
-        else executaNTransicoes (concatenarDuasListasSemRepeticao (executaTransicao estadosAtuais transicoes) estadosAtuais) transicoes
+executaNTransicoes estadosAtuais transicoes 
+    | transicaoExecutada == [] = []
+    | listasIguais listasConcatenadas  estadosAtuais = estadosAtuais
+    | otherwise = executaNTransicoes listasConcatenadas transicoes
+    where 
+        transicaoExecutada = executaTransicao estadosAtuais transicoes
+        listasConcatenadas = concatenarDuasListasSemRepeticao transicaoExecutada estadosAtuais
+
+
+--TODO:checar o caso do aceita vazio
+checaVariavelValida :: [Int] -> [Int] -> Bool -> Bool
+checaVariavelValida verificacaoVariavel estadosAtuais aceitaVazio
+    | estadosAtuais == [] = aceitaVazio
+    | listasIguais listaElementosDentro estadosAtuais = True 
+    | otherwise = False  
+    where 
+        listaElementosDentro = [x | x <- estadosAtuais, elem x verificacaoVariavel]
+
+
+
--- teste.hs
+++ teste.hs
@@ -4,18 +4,53 @@ import Utils
 import PDL
 
 estados = [1, 2, 3, 4, 5]
+
+-- transicao :: Eq a => a -> [(a, a)]
+-- transicao programa 
+--     | programa == "alpha" = [(1, 1), (1, 2), (2, 3), (3, 4), (4, 4)]
+--     | programa ==  "beta" = [(1, 2), (2, 1), (3, 3), (5, 5), (5, 4)]
+--     | otherwise = error "trasição não definida"
+
 transicaoAlpha  = [(1, 1), (1, 2), (2, 3), (3, 4), (4, 4)]
 transicaoBeta = [(1, 2), (2, 1), (3, 3), (5, 5), (5, 4)]
 -- transicoes vao ser uma funcao
 
+-- verificacaoVariavel :: Eq -> [Eq]
+-- verificacaoVariavel variavel 
+--     | variavel == "p" = [1, 2, 3, 4]
+--     | variavel == "q" = [1, 5]
+--     | otherwise = error "variável não definida"
+
+validaP = [1, 2, 3, 4]
+validaQ = [1, 5]
+
+
 estadoAtual = [1]
+
 
 t1 = executaTransicao estadoAtual transicaoAlpha
 t2 = executaTransicao [3] transicaoAlpha
-t3 = executaNTransicoes [1] transicaoAlpha
-t4  = executaNTransicoes [1] transicaoBeta
+
+t31 = executaTransicao [1] transicaoAlpha
+t32 = executaNTransicoes [1] transicaoAlpha
+
+t41  = executaTransicao [1] transicaoBeta
+t42  = executaNTransicoes [1] transicaoBeta
+
+t61 = executaTransicao [4] transicaoAlpha
+t62 = executaNTransicoes [4] transicaoAlpha
+
+t51 = executaTransicao [4] transicaoBeta
+t52 = executaNTransicoes [4] transicaoBeta
 
 
+w1 = checaVariavelValida validaP [1] False
+w2 = checaVariavelValida validaQ [1] False
+
+w3 = checaVariavelValida validaP [1, 5] False
+w4 = checaVariavelValida validaQ [5] False
+
+w5 = checaVariavelValida validaP [1, 2, 3, 4] False
 
 
 
