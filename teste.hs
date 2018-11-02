3dcc9ff1cdafd7aadd2294f5716013b9661b8ec8
sevontheedge <sivthiago@hotmail.com>
2018-11-02 18:02:15 -0300

    funcao com loop

--- teste.hs
+++ teste.hs
@@ -1,10 +1,11 @@
 import Data.List
 import System.IO
+import Data.List.Split
 import Utils
 import PDL
 
-
 estados = ['a', 'b', 'c']
+estadoAtual = head estados
 transicao :: Char -> [(Char, Char)]
 transicao aresta
     | aresta == 'α' = [('a', 'a'), ('a', 'b')]
@@ -14,8 +15,33 @@ transicao aresta
 programa = ["α;", "α U β", "β*"]
 
 
+containsEdge ::  String -> Char -> Bool
+containsEdge programa digito = digito `elem` programa
 
 
+
+checarExecutaTransicao :: Char -> [Char] -> [Char]
+checarExecutaTransicao paramTransicao estadosAtuais
+    | elem '∅' novosEstados = error "Transição com falhou para os estados atuais " ++ estadosAtuais 
+    | otherwise = novosEstados
+    where
+        novosEstados = executaTransicao estadosAtuais (transicao paramTransicao)
+
+
+executaLoop :: [String] -> [Char] -> Bool
+executaLoop [] estadoAtual = True
+executaLoop (x:xs) estadoAtual
+    | containsEdge x ';' =  executaLoop xs (checarExecutaTransicao (head x)  estadoAtual) 
+	| containsEdge x 'U' =   False
+    | containsEdge x '*' = False
+    | otherwise = error "Programa inválido"
+    
+
+  
+
+
+    -- splitOn "," "my,comma,separated,list"
+    -- ["my","comma","separated","list"]
 
 
 
