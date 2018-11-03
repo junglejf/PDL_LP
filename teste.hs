b8d45d41eb18a38e3bad4d3676ab746a43423392
assadfelipe <felipeassad@id.uff.br>
2018-11-02 22:36:17 -0300

    Descobri porque o erro nao eh lançado

--- teste.hs
+++ teste.hs
@@ -1,8 +1,14 @@
 import Data.List
 import System.IO
-import Data.List.Split
 import Utils
 import PDL
+import Debug.Trace
+
+
+
+foo a b c = trace ("input a: " ++ show a) (a + b + c)
+main = print (foo 1 2 3)
+
 
 estados = ['a', 'b', 'c']
 estadoAtual = head estados
@@ -28,16 +34,24 @@ checarExecutaTransicao paramTransicao estadosAtuais
         novosEstados = executaTransicao estadosAtuais (transicao paramTransicao)
 
 
+checarExecutaTransicao2 :: Char -> [Char] -> Bool
+checarExecutaTransicao2 paramTransicao estadosAtuais
+    | elem '∅' novosEstados = False
+    | otherwise = True
+    where
+        novosEstados = executaTransicao estadosAtuais (transicao paramTransicao)
+
+
+
 executaLoop :: [String] -> [Char] -> Bool
 executaLoop [] estadoAtual = True
 executaLoop (x:xs) estadoAtual
-    | containsEdge x ';' =  executaLoop xs (checarExecutaTransicao (head x)  estadoAtual) 
-	| containsEdge x 'U' =   False
+    | containsEdge x ';' =  (checarExecutaTransicao2 (head x)  estadoAtual) && executaLoop xs (checarExecutaTransicao (head x)  estadoAtual) 
+    | containsEdge x 'U' =   False
     | containsEdge x '*' = False
     | otherwise = error "Programa inválido"
-    
 
-  
+
 
 
     -- splitOn "," "my,comma,separated,list"
